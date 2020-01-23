f2efd6529676d095038a149ef4ded520cd17d66a
hurben <hurben@bioinfo@gmail.com>
2020-01-23 10:09:12 -0600

    update

--- important_examples/run_heatmaps.sh
+++
@@ -1,5 +0,0 @@
-file_dir=/Users/m221138/Scarisbrick_Project/RNAseq/analysis/gene_list_v1/
-while read p;
-do
-	Rscript /Users/m221138/RNAseq_pipeline/analysis/draw_pheatmap_klk6.r $file_dir$p
-done <interest.list
