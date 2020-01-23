605c7223b3a4bb099a93db15254ea4af3612667c
hurben <hurben@bioinfo@gmail.com>
2020-01-23 09:20:59 -0600

    update

--- analysis/draw_pheatmap_klk6_sex_adj.r
+++ analysis/draw_pheatmap_klk6_sex_adj.r
@@ -53,20 +53,24 @@ get_subset_df <- function(df, gene_list)
 #Get Intersection sets (Gene symbol) from three different inputs 
 #Might have been wise to do this process with python...
 df <- get_df('/Users/m221138/Scarisbrick_Project/RNAseq/deseq2/klk6.overall.deg.csv')
+s_df <- get_df('/Users/m221138/Scarisbrick_Project/RNAseq/deseq2/klk6.overall.deg.sex.adj.csv')
 m_df <- get_df('/Users/m221138/Scarisbrick_Project/RNAseq/deseq2/klk6.male.deg.csv')
 f_df <- get_df('/Users/m221138/Scarisbrick_Project/RNAseq/deseq2/klk6.female.deg.csv')
 
 df <- remove_na(df)
+s_df <- remove_na(s_df)
 m_df <- remove_na(m_df)
 f_df <- remove_na(f_df)
     
 df_gene_list <- row.names(df)
+s_df_gene_list <- row.names(s_df)
 f_df_gene_list <- row.names(f_df)
 m_df_gene_list <- row.names(m_df)
 
-common_gene_list <- intersect(intersect(df_gene_list,m_df_gene_list),f_df_gene_list)
+common_gene_list <- intersect(intersect(intersect(df_gene_list,m_df_gene_list),f_df_gene_list),s_df_gene_list)
 
 df <- get_subset_df(df, common_gene_list)
+s_df <- get_subset_df(s_df, common_gene_list)
 f_df <- get_subset_df(f_df, common_gene_list)
 m_df <- get_subset_df(m_df, common_gene_list)
 
@@ -82,13 +86,15 @@ print (output_file)
 print (length(predefined_gene_list))
 
 subset_df <- get_subset_df(df, predefined_gene_list)
+subset_s_df <- get_subset_df(s_df, predefined_gene_list)
 subset_f_df <- get_subset_df(f_df, predefined_gene_list)
 subset_m_df <- get_subset_df(m_df, predefined_gene_list)
 #==============================================================================
 
 
 #Making Barcodes to mark significant genes=====================================
-heatmap_ready_df <- data.frame(overall=subset_df$log2FoldChange, 
+heatmap_ready_df <- data.frame(sex_adj_overall=subset_s_df$log2FoldChange,
+							   overall=subset_df$log2FoldChange, 
                                female=subset_f_df$log2FoldChange,
                                male=subset_m_df$log2FoldChange)
 
@@ -98,16 +104,18 @@ gene_name_with_barcode_list <- c()
 
 for (gene_name in subset_df_gene_list)
 {
-    barcode_list <- c(0,0,0)
+    barcode_list <- c(0,0,0,0)
+    subset_s_df_pval <- subset_s_df[gene_name,"pvalue"]
     subset_df_pval <- subset_df[gene_name,"pvalue"]
     subset_f_df_pval <- subset_f_df[gene_name,"pvalue"]
     subset_m_df_pval <- subset_m_df[gene_name,"pvalue"]
     
-    barcode_list[1] <- get_barcode(subset_df_pval, pvalue_cutline)
-    barcode_list[2] <- get_barcode(subset_f_df_pval, pvalue_cutline)
-    barcode_list[3] <- get_barcode(subset_m_df_pval, pvalue_cutline)
+    barcode_list[1] <- get_barcode(subset_s_df_pval, pvalue_cutline)
+    barcode_list[2] <- get_barcode(subset_df_pval, pvalue_cutline)
+    barcode_list[3] <- get_barcode(subset_f_df_pval, pvalue_cutline)
+    barcode_list[4] <- get_barcode(subset_m_df_pval, pvalue_cutline)
 
-    gene_name_with_barcode <- paste(gene_name,'_', barcode_list[1], barcode_list[2],barcode_list[3],sep="")
+    gene_name_with_barcode <- paste(gene_name,'_', barcode_list[1], barcode_list[2],barcode_list[3],barcode_list[4],sep="")
     #print (gene_name_with_barcode)
     gene_name_with_barcode_list <- c(gene_name_with_barcode_list, gene_name_with_barcode)
 }
@@ -138,9 +146,7 @@ if (heatmap_size > 50 && heatmap_size < 100)
 	pheatmap(heatmap_ready_df, breaks=break_list, color=color,cluster_cols=FALSE, fontsize = 4, cellwidth=20)
 }
 if (heatmap_size >= 100)
-{
 	pheatmap(heatmap_ready_df, breaks=break_list, color=color,cluster_cols=FALSE, fontsize = 2, cellwidth=20)
-}
 dev.off()
 
 #================================================================
